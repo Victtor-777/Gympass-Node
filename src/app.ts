6f1991094c29e908f8830db0effc327e048ca0b2
Victtor777 <victtor.eng.software@gmail.com>
2024-08-02 19:49:23 -0300

    Fazendo o relacionamento entre as tabelas

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ export const app = fastify();
 
 const prisma = new PrismaClient();
 
-prisma.user.create({
-  data: {
-    name: "Diego Fernandes",
-    email: "diego@rocketseat.com.br",
-  },
-});
+// prisma.user.create({
+//   data: {
+//     name: "Diego Fernandes",
+//     email: "diego@rocketseat.com.br",
+//   },
+// });
