3984d0382bf4ee5768396d7d7677e49fac5cb441
Victtor777 <victtor.eng.software@gmail.com>
2024-08-12 18:07:41 -0300

    creating a middleware for authentication

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,15 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function profile(request: FastifyRequest, response: FastifyReply) {
-  await request.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  console.log(request.user.sub);
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
 
-  return response.status(200).send();
+  return response.status(200).send({
+    ...user,
+    password_hash: undefined,
+  });
 }
