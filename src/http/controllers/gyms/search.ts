69e939c3666000391878fcf0e67be9273c28ab9c
Victtor777 <victtor.eng.software@gmail.com>
2024-08-13 16:12:06 -0300

    Add controllers and routes for check-ins

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -8,7 +8,7 @@ export async function search(request: FastifyRequest, response: FastifyReply) {
     page: z.coerce.number().min(1).default(1),
   });
 
-  const { q, page } = searchGymsQuerySchema.parse(request.body);
+  const { q, page } = searchGymsQuerySchema.parse(request.query);
 
   const searchGymsUseCase = makeSearchGymsUseCase();
 
