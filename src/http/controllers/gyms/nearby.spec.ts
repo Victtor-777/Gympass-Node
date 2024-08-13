f7288b4fad40468b695b12870f031887ddb9c3ba
Victtor777 <victtor.eng.software@gmail.com>
2024-08-13 17:10:54 -0300

    Add test e2e for gym routes

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Nearby Gyms (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to list nearby gyms", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Some description",
+        phone: "991929394",
+        latitude: -15.7988836,
+        longitude: -47.8757631,
+      });
+
+    await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "TypeScript Gym",
+        description: "Some description",
+        phone: "991929394",
+        latitude: -15.8036594,
+        longitude: -47.9403266,
+      });
+
+    const response = await request(app.server)
+      .get("/gyms/nearby")
+      .query({
+        latitude: -15.7988836,
+        longitude: -47.8757631,
+      })
+      .set("Authorization", `Bearer ${token}`)
+      .send();
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.gyms).toHaveLength(1);
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: "JavaScript Gym",
+      }),
+    ]);
+  });
+});
